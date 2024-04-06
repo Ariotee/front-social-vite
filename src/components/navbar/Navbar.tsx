fd305bab8996dc77989e02fceb739d127eb2cac5
SoloTufi <mat_990011@mail.ru>
2024-04-06 14:31:24 +0700

    Create navbar Comp
    Signed-off-by: SoloTufi <mat_990011@mail.ru>

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,174 +1,51 @@
 import React from 'react';
 
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import style from './navbar.module.scss';
+import Logotype from './icons/Logotype';
+import Search from './icons/Search';
+import Input from '../input/Input';
 
 import AvatarMe from '../../assets/avatarMe.jpg';
 
-const pages = ['News', 'Friends', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-
-function Navbar() {
-	const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-	const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
-
-	const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-		setAnchorElNav(event.currentTarget);
-	};
-	const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-		setAnchorElUser(event.currentTarget);
-	};
-
-	const handleCloseNavMenu = () => {
-		setAnchorElNav(null);
-	};
-
-	const handleCloseUserMenu = () => {
-		setAnchorElUser(null);
-	};
-
+const Navbar: React.FC = () => {
 	return (
-		<AppBar position='static' sx={{ backgroundColor: '#1A1A1A' }}>
-			<Container maxWidth='lg'>
-				<Toolbar disableGutters>
-					<AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-					<Typography
-						variant='h6'
-						noWrap
-						component='a'
-						href='#'
-						sx={{
-							mr: 2,
-							display: { xs: 'none', md: 'flex' },
-							fontFamily: 'monospace',
-							fontWeight: 700,
-							color: 'inherit',
-							textDecoration: 'none',
-						}}
-					>
-						Connectify
-					</Typography>
+		<div className={style.root}>
+			<div className={style.container}>
+				<div className={style.flex}>
+					<div className={style.logo}>
+						<div className={style.logo__SVG}>
+							<Logotype />
+						</div>
+						<span className={style.logo__Text}>Connectify</span>
+					</div>
 
-					<Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-						<IconButton
-							size='large'
-							aria-label='account of current user'
-							aria-controls='menu-appbar'
-							aria-haspopup='true'
-							onClick={handleOpenNavMenu}
-							color='inherit'
-						>
-							<MenuIcon />
-						</IconButton>
-						<Menu
-							id='menu-appbar'
-							anchorEl={anchorElNav}
-							anchorOrigin={{
-								vertical: 'bottom',
-								horizontal: 'left',
-							}}
-							keepMounted
-							transformOrigin={{
-								vertical: 'top',
-								horizontal: 'left',
-							}}
-							open={Boolean(anchorElNav)}
-							onClose={handleCloseNavMenu}
-							sx={{
-								display: { xs: 'block', md: 'none' },
-							}}
-						>
-							{pages.map(page => (
-								<MenuItem key={page} onClick={handleCloseNavMenu}>
-									<Typography textAlign='center'>{page}</Typography>
-								</MenuItem>
-							))}
-						</Menu>
-					</Box>
+					<form className={style.form}>
+						<div className={style.bg}>
+							<label className={style.label}>Search</label>
+							<div className={style.input_body}>
+								<Search />
+								<Input />
+							</div>
+						</div>
+					</form>
 
-					<AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-					<Typography
-						variant='h5'
-						noWrap
-						component='a'
-						href='#'
-						sx={{
-							mr: 2,
-							display: { xs: 'flex', md: 'none' },
-							flexGrow: 1,
-							fontFamily: 'monospace',
-							fontWeight: 700,
-							color: 'inherit',
-							textDecoration: 'none',
-						}}
-					>
-						Connectify
-					</Typography>
+					<div className={style.avatar}>
+						<button className={style.avatarBtn}>
+							<img src={AvatarMe} alt='My avatar' className={style.avatarImg} />
+						</button>
+					</div>
 
-					<Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-						{pages.map(page => (
-							<Button
-								key={page}
-								onClick={handleCloseNavMenu}
-								sx={{
-									my: 2,
-									color: 'white',
-									display: 'block',
-									textTransform: 'capitalize',
-									':hover': {
-										bgcolor: '#1C1C1C',
-										filter: 'drop-shadow(1px 1px 5px #484848)',
-									},
-								}}
-							>
-								{page}
-							</Button>
-						))}
-					</Box>
-
-					<Box sx={{ flexGrow: 0 }}>
-						<Tooltip title='Open settings'>
-							<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-								<Avatar alt='Remy Sharp' src={AvatarMe} />
-							</IconButton>
-						</Tooltip>
-						<Menu
-							sx={{ mt: '45px', cursor: 'pointer' }}
-							id='menu-appbar'
-							anchorEl={anchorElUser}
-							anchorOrigin={{
-								vertical: 'top',
-								horizontal: 'right',
-							}}
-							keepMounted
-							transformOrigin={{
-								vertical: 'top',
-								horizontal: 'right',
-							}}
-							open={Boolean(anchorElUser)}
-							onClose={handleCloseUserMenu}
-						>
-							{settings.map(setting => (
-								<MenuItem key={setting} onClick={handleCloseUserMenu}>
-									<Typography textAlign='center'>{setting}</Typography>
-								</MenuItem>
-							))}
-						</Menu>
-					</Box>
-				</Toolbar>
-			</Container>
-		</AppBar>
+					{/*<nav className={style.nav}>
+						<ul className={style.nav__list}>
+							<li className={style.item}></li>
+							<li className={style.item}></li>
+							<li className={style.item}></li>
+							<li className={style.item}></li>
+						</ul>
+					</nav>*/}
+				</div>
+			</div>
+		</div>
 	);
-}
+};
 export default Navbar;
