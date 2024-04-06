1d983bf1f74b57439aaa891a200fa451a9daefd4
SoloTufi <mat_990011@mail.ru>
2024-04-06 22:17:07 +0700

    layout profile comp
    Signed-off-by: SoloTufi <mat_990011@mail.ru>

---
+++ src/components/profile/Profile.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import ProfileHeader from './components/profileHeader/ProfileHeader';
+import ProfileHeaderInfo from './components/profileHeaderInfo/ProfileHeaderInfo';
+
+import style from './profile.module.scss';
+
+const MyProfile: React.FC = () => {
+	return (
+		<>
+			<div className={style.flex}>
+				<ProfileHeader />
+				<ProfileHeaderInfo />
+			</div>
+		</>
+	);
+};
+
+export default MyProfile;
