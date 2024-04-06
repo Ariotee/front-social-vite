27f1d4b143864beae4246c2156969a0f4c4b1eb5
SoloTufi <mat_990011@mail.ru>
2024-04-06 22:16:28 +0700

    Layout Sidebar Comp
    Signed-off-by: SoloTufi <mat_990011@mail.ru>

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,11 +1,56 @@
 import React from 'react';
 
+import { Friends, Messenger, Music, MyProfile, News } from './Icons';
+
 import style from './sidebar.module.scss';
 
-export const LeftSidebar: React.FC = () => {
-	return <div></div>;
-};
-
-export const RightSidebar: React.FC = () => {
-	return <div></div>;
+export const LeftSideMenu: React.FC = () => {
+	return (
+		<aside className={style.root}>
+			<nav className={style.nav}>
+				<ul className={style.nav__list}>
+					<li className={style.item}>
+						<span className={style.item__icon}>
+							<MyProfile />
+						</span>
+						<a href='#' className={style.item__link}>
+							My profile
+						</a>
+					</li>
+					<li className={style.item}>
+						<span className={style.item__icon}>
+							<News />
+						</span>
+						<a href='#' className={style.item__link}>
+							News
+						</a>
+					</li>
+					<li className={style.item}>
+						<span className={style.item__icon}>
+							<Messenger />
+						</span>
+						<a href='#' className={style.item__link}>
+							Messenger
+						</a>
+					</li>
+					<li className={style.item}>
+						<span className={style.item__icon}>
+							<Friends />
+						</span>
+						<a href='#' className={style.item__link}>
+							Friends
+						</a>
+					</li>
+					<li className={style.item}>
+						<span className={style.item__icon}>
+							<Music />
+						</span>
+						<a href='#' className={style.item__link}>
+							Music
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</aside>
+	);
 };
